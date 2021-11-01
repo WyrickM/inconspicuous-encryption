afbf751166a764ed7d3b370053e3eb9e7ae66dfa
WyrickM <mantz.wyrick@wsu.edu>
2021-11-01 16:59:29 -0700

    as far as I can tell the encryption works, working on decryption

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,8 @@
-import logging
-import os
-import json
 import base64
+import cv2
+import logging
+import json
+import os
 from http import HTTPStatus
 from tkinter import filedialog
 
@@ -13,11 +14,11 @@ app = Flask(__name__)
 
 def text_to_binary(message):
     """converts the user's inputed message to binary"""
-    if isinstance(message) == str:
+    if isinstance(message, str):
         return "".join([format(ord(i), "08b") for i in message])
-    elif isinstance(message) == bytes or isinstance(message) == np.ndarray:
+    elif isinstance(message, bytes) or isinstance(message, np.ndarray):
         return [format(i, "08b") for i in message]
-    elif isinstance(message) == int or isinstance(message) == np.uint8:
+    elif isinstance(message, int) or isinstance(message, np.uint8):
         return format(message, "08b")
     else:
         raise TypeError("Input type not supported")
@@ -33,7 +34,7 @@ def hide_message(image, message):
     if len(message) > n_bytes:
         raise ValueError("Error: message length is too long for the image")
 
-    message += "####"
+    message += "~~~~~"
 
     # convert input data to binary
     binary_message = text_to_binary(message)
@@ -88,7 +89,7 @@ def show_message(image):
 
         # checking delimeter we put at the end of the original message,
         #  if so we decrypted the whole message
-        if decrypted_message[-4:] == "####":
+        if decrypted_message[-5:] == "~~~~~":
             break
 
     # backend check to test if done properly
@@ -97,63 +98,97 @@ def show_message(image):
     return decrypted_message[:-5]  # removing delimeter
 
 
-def save_file(image):
+def save_file(image, file_type):
     """opens the typical save dialog box to save the encrypted image"""
-    file = filedialog.asksaveasfile(
-        defaultextension=".jpg",
-        filetypes=[("JPG", ".jpg"), ("PNG", ".png"), ("PDF", ".pdf")],
-    )
-
-    # exception checking when users cancels save
-    if file is None:
-        return
-    file.write(image)
-    file.close()
+    # give the new image a name, notifying user that this is the new image and to change the name
+    file_name = "Secret_Message_CHANGE_NAME" + file_type
+    cv2.imwrite(file_name, image)
 
 
 def get_image(image):
+    """saves it locally so we can read it with cv2 as binary array"""
+    # splitting the image data from json blob
     image_data_list = image["file"]["thumbUrl"].split(",", 1)
+
+    # getting the base64 data, other part is not part of the image data
     base64_image = image_data_list[1]
     base64_image_bytes = base64_image.encode("utf-8")
     decoded_image_data = base64.decodebytes(base64_image_bytes)
+
+    # writing the temporary image to the current working directory
     temp_image = open(image["file"]["name"], "wb")
     temp_image.write(decoded_image_data)
     temp_image.close()
 
+
 def delete_temp_image(image):
-    
+    """Delete the temp_image that needed to be writen locally"""
+    try:
+        image_name = image["file"]["name"]
+        if os.path.exists(image_name):
+            os.remove(image_name)
+    except ValueError as error:
+        print("Error: Did not delete the temporary image is it existed: ", error)
+
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/encrypt/encrypt_message", methods=["POST"])
 def encrypt_message():
     """Encryption function that is called when button click on the frontend in the encrpyt page"""
+
+    # get the data from the frontend
     try:
         data = request.json
         image = data.get("image")
-        message = data.get("message")
+        image_name = image["file"]["name"]
         get_image(image)
+        message = data.get("message")
+        if len(message) == 0:
+            raise ValueError('Message is empty')
     except ValueError as error:
         logging.error(error)
         return str(error), HTTPStatus.BAD_REQUEST
 
 
-    # encrypted_image = hide_message(image2, message)
-    # save_file(encrypted_image)
+    # get the temp image and file type
+    temp_image = cv2.imread(image_name)
+    image_file_type = image_name[-4:]
+
+    # encrypt the message into the image
+    encrypted_image = hide_message(temp_image, message)
+
+    # save the new image that has encrypted message
+    save_file(encrypted_image, image_file_type)
+
+    # delete the temporary image
+    delete_temp_image(image)
+
     return ("Image saved!"), HTTPStatus.OK
 
 
 @app.route("/decrypt/decrypt_message", methods=["POST"])
 def decrypt_message():
     """Decryption function that is called when button click on the frontend in the decrypt page"""
+
+    # get the data from the frontend
     try:
         data = request.json
         image = data.get("image")
+        image_name = image["file"]["name"]
+        get_image(image)
     except ValueError as error:
         logging.error(error)
         return str(error), HTTPStatus.BAD_REQUEST
 
-    text = show_message(image)
-    return (jsonify({"message": text}), HTTPStatus.OK)
+    # get temp steganography image
+    temp_image = cv2.imread(image_name)
+
+    # decrypt the message from the image
+    decrypted_message = show_message(temp_image)
+
+    print("Decrypted message: ", decrypted_message)
+
+    return (jsonify({"message": decrypted_message}), HTTPStatus.OK)
 
 
 if __name__ == "__main__":
