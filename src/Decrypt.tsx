c3660d8a8a5d7f8cb5dc5c5acad8eb49aa11314a
WyrickM <mantz.wyrick@wsu.edu>
2021-11-22 12:36:25 -0800

    fixed the need to double click the encrypt and decrypt buttons

--- src/Decrypt.tsx
+++ src/Decrypt.tsx
@@ -1,17 +1,17 @@
 import { Button, Card, Form, message, PageHeader, Typography, Upload } from "antd";
 import * as React from "react";
 import { InboxOutlined } from '@ant-design/icons';
-import {decryptMessage} from "./EncryptDecrypt";
+import { decryptMessage } from "./EncryptDecrypt";
 
 
-const {Dragger} = Upload
-const {Title} = Typography
+const { Dragger } = Upload
+const { Title } = Typography
 
 interface DecryptFormData {
     image: File;
 }
 
-const Decrypt:React.FC = () => {
+const Decrypt: React.FC = () => {
 
     const [formData, setFormData] = React.useState<DecryptFormData | undefined>(undefined);
     const [decryptedMessage, setDecryptedMessage] = React.useState<string | undefined>(undefined);
@@ -22,12 +22,11 @@ const Decrypt:React.FC = () => {
     }
 
     const onChange = (info: any) => {
-        const {status} = info.file;
-        if(status !== 'uploading') {
+        const { status } = info.file;
+        if (status !== 'uploading') {
             console.log(info.file, info.fileList);
         }
-        if(status === 'done')
-        {
+        if (status === 'done') {
             message.success(`${info.file.name} file upload failed.`);
         }
     };
@@ -38,7 +37,7 @@ const Decrypt:React.FC = () => {
 
     const onPreview = async (file: any) => {
         let src = file.url;
-        if(!src){
+        if (!src) {
             src = await new Promise(resolve => {
                 const reader = new FileReader();
                 reader.readAsDataURL(file.originFileObj);
@@ -52,12 +51,11 @@ const Decrypt:React.FC = () => {
     };
 
     const decrypt = async () => {
-        if(formData !== undefined)
-        {
+        if (formData !== undefined) {
             const r = await decryptMessage(
                 formData.image
             );
-            if(r.status >= 400) {
+            if (r.status >= 400) {
                 const text = await r.text();
                 message.error(`Decrypting the image failed: ${text}`);
             }
@@ -69,44 +67,48 @@ const Decrypt:React.FC = () => {
         }
     }
 
+    React.useEffect(() => {
+        onFinish(formData!);
+    }, [formData]);
+
 
     return (
         <>
             <PageHeader
-                title= {
-                    <Title style= {{ fontSize: "50px" }}>
+                title={
+                    <Title style={{ fontSize: "50px" }}>
                         Decrypt
                     </Title>
                 }
                 className="main-content"
             >
-                <div 
+                <div
                     style={{
                         fontSize: "25px",
-                        paddingBottom:15
+                        paddingBottom: 15
                     }}
                 >
                     This page is where you will decrypt a message that is inside a picture so you can inconspicuosly hide
                     a secret message. The after uploading an image, if there is an encrypted message that was encrypted with
                     the Inconspicuous Encryption encryption algorithm the message will appear below.
-                    <br/>
-                    <br/>
+                    <br />
+                    <br />
                     NOTE: It is important to only try to decrypt messages that were encrypted using the Inconspicuous Encryption encryption algorithm.
                     If the image has a secret message from a different steganography application it may not produce the proper message.
                 </div>
                 <Card>
                     <Form
-                        onFinish={(items) => {onFinish(items)}}
+                        onFinish={(items) => { onFinish(items) }}
                     >
                         <Form.Item name="image" required={true}>
-                            <Dragger 
-                                name= 'file'
+                            <Dragger
+                                name='file'
                                 accept=".jpg, .png"
                                 listType="picture"
                                 maxCount={1}
-                                onChange={(e) => {onChange(e);}}
-                                onDrop={(e) => {onDrop(e);}}
-                                onPreview={(file) => {onPreview(file);}}
+                                onChange={(e) => { onChange(e); }}
+                                onDrop={(e) => { onDrop(e); }}
+                                onPreview={(file) => { onPreview(file); }}
                             >
                                 <p className="ant-upload-drag-icon">
                                     <InboxOutlined />
@@ -119,8 +121,8 @@ const Decrypt:React.FC = () => {
                                 </p>
                             </Dragger>
                         </Form.Item>
-                        <Form.Item>
-                            <Button 
+                        <Form.Item name="button">
+                            <Button
                                 type="primary"
                                 htmlType="submit"
                             >
@@ -131,14 +133,14 @@ const Decrypt:React.FC = () => {
                     {decryptedMessage ? (
                         <div>
                             <p>
-                            The decrypted message is:
+                                The decrypted message is:
                             </p>
                             <p>
                                 {decryptedMessage}
                             </p>
                         </div>
-                        
-                    ): null}
+
+                    ) : null}
                 </Card>
             </PageHeader>
         </>
--- src/Encrypt.tsx
+++ src/Encrypt.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { Button, Card, Form, Input, message, PageHeader, Typography, Upload } from "antd";
-import { InboxOutlined  } from '@ant-design/icons';
-import {encryptMessage} from "./EncryptDecrypt";
+import { InboxOutlined } from '@ant-design/icons';
+import { encryptMessage } from "./EncryptDecrypt";
 
 
-const {Dragger} = Upload
-const {Title} = Typography
+const { Dragger } = Upload
+const { Title } = Typography
 
 interface EncryptFormData {
     image: File;
@@ -13,21 +13,20 @@ interface EncryptFormData {
 }
 
 
-const Encrypt:React.FC = () => {
+const Encrypt: React.FC = () => {
     const [formData, setFormData] = React.useState<EncryptFormData | undefined>(undefined);
 
     const onFinish = (items: EncryptFormData) => {
         setFormData(items);
         encrypt();
     }
-    
+
     const onChange = (info: any) => {
-        const {status} = info.file;
-        if(status !== 'uploading') {
+        const { status } = info.file;
+        if (status !== 'uploading') {
             console.log(info.file, info.fileList);
         }
-        if(status === 'done')
-        {
+        if (status === 'done') {
             message.success(`${info.file.name} file upload failed.`);
         }
     };
@@ -38,7 +37,7 @@ const Encrypt:React.FC = () => {
 
     const onPreview = async (file: any) => {
         let src = file.url;
-        if(!src){
+        if (!src) {
             src = await new Promise(resolve => {
                 const reader = new FileReader();
                 reader.readAsDataURL(file.originFileObj);
@@ -53,58 +52,62 @@ const Encrypt:React.FC = () => {
 
     const encrypt = async () => {
 
-        if(formData !== undefined)
-        {
+        if (formData !== undefined) {
             const r = await encryptMessage(
                 formData.image,
                 formData.message
             );
-            if(r.status >= 400) {
+            if (r.status >= 400) {
                 const text = await r.text();
-                message.error(`Encrypting the image failed: ${text}` );
+                message.error(`Encrypting the image failed: ${text}`);
             }
             else {
                 message.success("Encryting the image succeeded!");
             }
         }
-        
-    }
+    };
+
+    React.useEffect(() => {
+        onFinish(formData!);
+    }, [formData]);
+
+
     return (
         <>
             <PageHeader
-                title= {
-                    <Title style={{fontSize: "50px"}}>
+                title={
+                    <Title style={{ fontSize: "50px" }}>
                         Encrypt
                     </Title>
                 }
                 className="main-content"
             >
-                <div 
+                <div
                     style={{
                         fontSize: "25px",
-                        paddingBottom:15
+                        paddingBottom: 15
                     }}
                 >
                     This page is where you will encrypt a message inside a picture so you can inconspicuosly hide
                     a secret message. What seems to be an identical image of the one that was uploaded will be downloaded
                     to your machine, but with your secret message. Then you can send your message securely any way you desire.
-                    <br/>
-                    <br/>
-                    NOTE: The new image will be saved as a PNG file type no matter the given image type. This is because JPG files 
+                    <br />
+                    <br />
+                    NOTE: The new image will be saved as a PNG file type no matter the given image type. This is because JPG files
                     are lossy formats. And it is important to not lose any bits since they hold the secret message.
                 </div>
 
                 <Card>
-                    <Form onFinish={(items) => {onFinish(items)}}>
+                    <Form onFinish={(items) => { onFinish(items) }}>
                         <Form.Item name="image" required={true}>
-                            <Dragger 
-                                name= 'file'
+                            <Dragger
+                                name='file'
                                 accept=".jpg, .png"
                                 listType="picture"
                                 maxCount={1}
-                                onChange={(e) => {onChange(e);}}
-                                onDrop={(e) => {onDrop(e);}}
-                                onPreview={(file) => {onPreview(file);}}
+                                onChange={(e) => { onChange(e); }}
+                                onDrop={(e) => { onDrop(e); }}
+                                onPreview={(file) => { onPreview(file); }}
                             >
                                 <p className="ant-upload-drag-icon">
                                     <InboxOutlined />
@@ -117,16 +120,16 @@ const Encrypt:React.FC = () => {
                                 </p>
                             </Dragger>
                         </Form.Item>
-                        
+
                         <Form.Item
                             label="Message"
                             name="message"
                             required={true}
-                            style={{paddingTop: 15}}
+                            style={{ paddingTop: 15 }}
                         >
                             <Input.TextArea />
                         </Form.Item>
-                        <Form.Item>
+                        <Form.Item name="button">
                             <Button type="primary" htmlType="submit">Encrypt</Button>
                         </Form.Item>
                     </Form>
